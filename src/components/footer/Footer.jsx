a8cb95fbdb8ade6752557166b01870da7c02a8d0
shaikat17 <shaikatpal56@gmail.com>
2023-07-19 00:47:31 +0600

    Footer section done.

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,10 +1,16 @@
-import React from "react";
+import Image from 'next/image';
+import styles from './footer.module.css'
 
 const Footer = () => {
   return (
-    <div>
-      <div>Made With <span style={{"color": "red"}}>&hearts;</span></div>
-      <div></div>
+    <div className={styles.container}>
+      <div>Made by Shaikat With <span style={{"color": "red"}}>&hearts;</span></div>
+      <div className={styles.social}>
+      <Image src="/1.png" width={15} height={15} className={styles.icon} alt="Shaikat Facebook Account" />
+        <Image src="/2.png" width={15} height={15} className={styles.icon} alt="Shaikat" />
+        <Image src="/3.png" width={15} height={15} className={styles.icon} alt="Shaikat" />
+        <Image src="/4.png" width={15} height={15} className={styles.icon} alt="Shaikat" />
+      </div>
     </div>
   );
 };
